65d91dbe15a7b967e7874d6e17d874b382ac98b0
Sean Nessen <scnessen@gmail.com>
2020-05-25 17:16:46 -0700

    Do not start a snapshot if one is already pending

--- snapshotanalyzer/snapshotanalyzer.py
+++ snapshotanalyzer/snapshotanalyzer.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volumes.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """snapshotanalyzer manages snapshots"""
@@ -91,6 +95,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("  Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+
             print("  Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshotanalyzer 30000")
 
